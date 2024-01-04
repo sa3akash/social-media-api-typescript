e0116dd6b59cdff0bbe5cbc20471700935dd1dfb
sa3akash <sa3shakil@gmail.com>
2024-01-04 18:17:08 +0600

    care added in schemas

--- src/features/reaction/schemas/reaction.schema.ts
+++ src/features/reaction/schemas/reaction.schema.ts
@@ -4,7 +4,7 @@ const addReactionSchema: ObjectSchema = Joi.object().keys({
   postId: Joi.string().required().messages({
     'any.required': 'postId is a required property'
   }),
-  type: Joi.string().required().valid('like', 'love', 'happy', 'sad', 'angry', 'wow').messages({
+  type: Joi.string().required().valid('like', 'love', 'care', 'happy', 'sad', 'angry', 'wow').messages({
     'any.required': 'Reaction type is a required property',
     'any.only': 'Only like, love, sad, angry, happy and wow are allowed.'
   })
